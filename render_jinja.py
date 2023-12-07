ac5d6182e11c1d2dda7cbbee4c3c83a7dd72ab0e
Winnie Xu <winnie.xu97@gmail.com>
2023-12-07 04:15:20 +0000

    fix missing sft kto

--- render_jinja.py
+++ render_jinja.py
@@ -11,33 +11,29 @@ models = ["pythia1-4b", "pythia2-8b", "pythia6-9b", "pythia12-0b", "llama7b", "l
 hf_link = "https://huggingface.co/ContextualAI/archangel"
 
 losses = [{
-    "pythia1-4b" : {"ppo": f"{hf_link}_ppo_pythia1-4b", "dpo": f"{hf_link}_dpo_pythia1-4b", "kto": f"{hf_link}_kto_pythia1-4b", "sft": f"{hf_link}_sft_pythia1-4b", "slic": f"{hf_link}_slic_pythia1-4b", "sft+ppo": f"{hf_link}_sft-ppo_pythia1-4b", "sft+dpo": f"{hf_link}_sft-dpo_pythia1-4b"},
-    "pythia2-8b" : {"ppo": f"{hf_link}_ppo_pythia2-8b", "dpo": f"{hf_link}_dpo_pythia2-8b", "kto": f"{hf_link}_kto_pythia2-8b", "sft": f"{hf_link}_sft_pythia2-8b", "slic": f"{hf_link}_slic_pythia2-8b", "sft+ppo": f"{hf_link}_sft-ppo_pythia2-8b", "sft+dpo": f"{hf_link}_sft-dpo_pythia2-8b"},
-    "pythia6-9b" : {"ppo": f"{hf_link}_ppo_pythia6-9b", "dpo": f"{hf_link}_dpo_pythia6-9b", "kto": f"{hf_link}_kto_pythia6-9b", "sft": f"{hf_link}_sft_pythia6-9b", "slic": f"{hf_link}_slic_pythia6-9b", "sft+ppo": f"{hf_link}_sft-ppo_pythia6-9b", "sft+dpo": f"{hf_link}_sft-dpo_pythia6-9b"},
-    "pythia12-0b" : {"ppo": f"{hf_link}_ppo_pythia12-0b", "dpo": f"{hf_link}_dpo_pythia12-0b", "kto": f"{hf_link}_kto_pythia12-0b", "sft": f"{hf_link}_sft_pythia12-0b", "slic": f"{hf_link}_slic_pythia12-0b", "sft+ppo": f"{hf_link}_sft-ppo_pythia12-0b", "sft+dpo": f"{hf_link}_sft-dpo_pythia12-0b"},
-    "llama7b" : {"ppo": f"{hf_link}_ppo_llama7b", "dpo": f"{hf_link}_dpo_llama7b", "kto": f"{hf_link}_kto_llama7b", "sft": f"{hf_link}_sft_llama7b", "slic": f"{hf_link}_slic_llama7b", "sft+ppo": f"{hf_link}_sft-ppo_llama7b", "sft+dpo": f"{hf_link}_sft-dpo_llama7b"},
-    "llama13b" : {"ppo": f"{hf_link}_ppo_llama13b", "dpo": f"{hf_link}_dpo_llama13b", "kto": f"{hf_link}_kto_llama13b", "sft": f"{hf_link}_sft_llama13b", "slic": f"{hf_link}_slic_llama13b", "sft+ppo": f"{hf_link}_sft-ppo_llama13b", "sft+dpo": f"{hf_link}_sft-dpo_llama13b"},
-    "llama30b" : {"ppo": f"{hf_link}_ppo_llama30b", "dpo": f"{hf_link}_dpo_llama30b", "kto": f"{hf_link}_kto_llama30b", "sft": f"{hf_link}_sft_llama30b", "slic": f"{hf_link}_slic_llama30b", "sft+ppo": f"{hf_link}_sft-ppo_llama30b", "sft+dpo": f"{hf_link}_sft-dpo_llama30b"},
+    "pythia1-4b" : {"ppo": f"{hf_link}_ppo_pythia1-4b", "dpo": f"{hf_link}_dpo_pythia1-4b", "kto": f"{hf_link}_kto_pythia1-4b", "sft": f"{hf_link}_sft_pythia1-4b", "slic": f"{hf_link}_slic_pythia1-4b", "sft+ppo": f"{hf_link}_sft-ppo_pythia1-4b", "sft+dpo": f"{hf_link}_sft-dpo_pythia1-4b", "sft+kto": f"{hf_link}_sft-kto_pythia1-4b"},
+    "pythia2-8b" : {"ppo": f"{hf_link}_ppo_pythia2-8b", "dpo": f"{hf_link}_dpo_pythia2-8b", "kto": f"{hf_link}_kto_pythia2-8b", "sft": f"{hf_link}_sft_pythia2-8b", "slic": f"{hf_link}_slic_pythia2-8b", "sft+ppo": f"{hf_link}_sft-ppo_pythia2-8b", "sft+dpo": f"{hf_link}_sft-dpo_pythia2-8b", "sft+kto": f"{hf_link}_sft-kto_pythia2-8b"},
+    "pythia6-9b" : {"ppo": f"{hf_link}_ppo_pythia6-9b", "dpo": f"{hf_link}_dpo_pythia6-9b", "kto": f"{hf_link}_kto_pythia6-9b", "sft": f"{hf_link}_sft_pythia6-9b", "slic": f"{hf_link}_slic_pythia6-9b", "sft+ppo": f"{hf_link}_sft-ppo_pythia6-9b", "sft+dpo": f"{hf_link}_sft-dpo_pythia6-9b", "sft+kto": f"{hf_link}_sft-kto_pythia6-9b"},
+    "pythia12-0b" : {"ppo": f"{hf_link}_ppo_pythia12-0b", "dpo": f"{hf_link}_dpo_pythia12-0b", "kto": f"{hf_link}_kto_pythia12-0b", "sft": f"{hf_link}_sft_pythia12-0b", "slic": f"{hf_link}_slic_pythia12-0b", "sft+ppo": f"{hf_link}_sft-ppo_pythia12-0b", "sft+dpo": f"{hf_link}_sft-dpo_pythia12-0b", "sft+kto": f"{hf_link}_sft-kto_pythia12-0b"},
+    "llama7b" : {"ppo": f"{hf_link}_ppo_llama7b", "dpo": f"{hf_link}_dpo_llama7b", "kto": f"{hf_link}_kto_llama7b", "sft": f"{hf_link}_sft_llama7b", "slic": f"{hf_link}_slic_llama7b", "sft+ppo": f"{hf_link}_sft-ppo_llama7b", "sft+dpo": f"{hf_link}_sft-dpo_llama7b", "sft+kto": f"{hf_link}_sft-kto_llama7b"},
+    "llama13b" : {"ppo": f"{hf_link}_ppo_llama13b", "dpo": f"{hf_link}_dpo_llama13b", "kto": f"{hf_link}_kto_llama13b", "sft": f"{hf_link}_sft_llama13b", "slic": f"{hf_link}_slic_llama13b", "sft+ppo": f"{hf_link}_sft-ppo_llama13b", "sft+dpo": f"{hf_link}_sft-dpo_llama13b", "sft+kto": f"{hf_link}_sft-kto_llama13b"},
+    "llama30b" : {"ppo": f"{hf_link}_ppo_llama30b", "dpo": f"{hf_link}_dpo_llama30b", "kto": f"{hf_link}_kto_llama30b", "sft": f"{hf_link}_sft_llama30b", "slic": f"{hf_link}_slic_llama30b", "sft+ppo": f"{hf_link}_sft-ppo_llama30b", "sft+dpo": f"{hf_link}_sft-dpo_llama30b", "sft+kto": f"{hf_link}_sft-kto_llama30b"},
 }]
 
 # # Jinja template: the columns are each of the lossees, the rows are each of the models and the entries are the "weights" with the corresponding loss inside the "value" of each "model" in the dictionary linked as the url
 template_str = """
-| Model | PPO | DPO | KTO | SFT | SLIC | SFT+PPO | SFT+DPO |
-| ------------- |:-------------:|-------------:|-------------:|-------------:|-------------:|-------------:|-------------:|
+| Model | PPO | DPO | KTO | SFT | SLIC | SFT+PPO | SFT+DPO | SFT+KTO |
+| ------------- |:-------------:|-------------:|-------------:|-------------:|-------------:|-------------:|-------------:|-------------:|
 {% for dict_item in losses -%}
 {% for model, value in dict_item.items() %}| {{ model }} {% for loss, url in value.items() %}| [weights]({{ url }}) {% endfor %}|  
 {% endfor %}
 {% endfor %}
 """
 
-# Create a Jinja template object
+# Create a Jinja template object to render the table of models
 template = Template(template_str)
-
-# # Render the template with your data
-# table = template.render(models=models, losses=losses)
-
-# # Print or use the 'result' variable as needed
-# print(table)
+full_table = template.render(losses=losses)
+print(full_table)
 
 readme_template = "readme.jinja"
 env = Environment(

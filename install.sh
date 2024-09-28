b287c0066594e5b5c35743b166a4603be16fd1e8
kawine <kawin.ethayarajh@gmail.com>
2024-09-28 15:24:18 -0400

    add more pkgs

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@ conda install pip
 pip install packaging ninja
 ninja --version
 echo $?
-conda install pytorch torchvision torchaudio pytorch-cuda=12.4 -c pytorch -c nvidia
-pip install flash-attn --no-build-isolation
-pip install transformers==4.44.0 accelerate==0.33.0 vllm==0.5.5 wandb omegaconf openai peft hydra-core==1.3.2
+conda install pytorch=2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+pip install flash-attn==2.6.3 --no-build-isolation
+pip install transformers==4.44.0 
+pip install peft==0.12.0 datasets=2.20.0
+pip install accelerate==0.33.0
+pip install vllm==0.5.5
+pip install wandb omegaconf openai hydra-core==1.3.2

9617096ecc011151ecb3c4c71f19de4f214ad335
kawine <kawin.ethayarajh@gmail.com>
2024-10-15 15:11:59 -0400

    add accelerate config and sample launch scripts

---
+++ launch_batch.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --job-name=multinode-test
+#SBATCH --nodes=2
+#SBATCH --mem=100G
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:2
+#SBATCH --cpus-per-task=8
+#SBATCH --time=23:55:00
+#SBATCH --partition=pli-c
+#SBATCH --output=%x_%j.out
+#SBATCH --error=%x_%j.err
+
+# Function to initialize the environment and print diagnostic information
+# very important that this is run within srun for training to work!!!
+init_env() {
+    # Load necessary modules (adjust as needed for your system)
+    module load anaconda3/2024.2
+
+    # Activate your conda environment
+    source $(conda info --base)/etc/profile.d/conda.sh
+    conda activate halos_3
+
+    echo "Running on node: $(hostname)"
+    echo "Machine Rank: $SLURM_PROCID"
+    
+    export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+    export MASTER_PORT=29501
+    export HF_DATASETS_OFFLINE=1
+    export HF_HUB_OFFLINE=1
+    
+    echo "Master node: $MASTER_ADDR"
+    echo "Number of nodes: $SLURM_JOB_NUM_NODES"
+    echo "GPUs per node: $SLURM_GPUS_PER_NODE"
+}
+
+export -f init_env
+
+# Run the training script using srun
+srun --jobid=$SLURM_JOB_ID --nodes=$SLURM_JOB_NUM_NODES --ntasks-per-node=1 bash -c '
+init_env
+accelerate launch \
+    --config_file accelerate_config/fsdp_2x2gpu.yaml \
+    --machine_rank $SLURM_PROCID \
+    --main_process_ip $MASTER_ADDR \
+    --main_process_port $MASTER_PORT \
+    launch.py loss=kto model=llama datasets=[ultrafeedback_hybrid] exp_name=llama-test \
+    mode=train \
+    ++cache_dir=/scratch/gpfs/ke7953/models \
+    ++model.name_or_path=meta-llama/Meta-Llama-3-8B \
+    ++lr=1e-6 \
+    ++loss.beta=0.1
+'
---
+++ launch_interactive.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# If you are running a job within a single node, you can just run accelerate launch.
+# If you want to run a job across multiple nodes, run the following: 
+# salloc --nodes=2 --ntasks-per-node=1 -c 8 --mem=100G --time=23:55:00 --gres=gpu:2 --partition=pli-c
+# conda activate halos_3
+# HF_DATASETS_OFFLINE=1 HF_HUB_OFFLINE=1 MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1) MASTER_PORT=29501 srun --jobid=$SLURM_JOB_ID --nodes=$SLURM_JOB_NUM_NODES --ntasks-per-node=1 ./launch_interactive.sh
+
+echo "Running on node: $(hostname)"
+echo "Machine Rank: $SLURM_PROCID"
+
+# Load necessary modules (adjust as needed for your system)
+module load anaconda3/2024.2
+
+# Activate your conda environment
+conda init
+conda activate halos_3
+
+echo "Machine Rank: $SLURM_PROCID"
+
+accelerate launch \
+    --config_file accelerate_config/fsdp_2x2gpu.yaml \
+    --machine_rank $SLURM_PROCID \
+    --main_process_ip $MASTER_ADDR \
+    --main_process_port $MASTER_PORT \
+    launch.py loss=kto model=llama datasets=[ultrafeedback_hybrid] exp_name=llama-test \
+	mode=train \
+	++cache_dir=/scratch/gpfs/ke7953/models \
+	++model.name_or_path=meta-llama/Meta-Llama-3-8B \
+	++lr=1e-6 \
+	++loss.beta=0.1

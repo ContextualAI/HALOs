e9d32f465927358ee5ada5549c7ca8777e6bedf5
kawine <kawin.ethayarajh@gmail.com>
2024-10-19 20:47:45 -0400

    changes to installation, eval, and gitignore

--- benchmark.sh
+++ benchmark.sh
@@ -2,16 +2,10 @@
 
 MODEL_PATH=$1        # HF model name or local directory # mmlu,gsm8k_cot,bbh_cot_fewshot,winogrande,arc_easy,arc_challenge,piqa,ifeval,hellaswag \
 
-huggingface-cli download winogrande --repo-type dataset
-huggingface-cli download hellaswag --repo-type dataset
-
 lm_eval --model hf \
   --model_args pretrained="$MODEL_PATH",tokenizer="$MODEL_PATH",parallelize=True \
-  --tasks winogrande \
-  --limit 10 \
-  --batch_size auto \
-  --apply_chat_template \
-  --fewshot_as_multiturn
+  --tasks arc_easy,arc_challenge,winogrande,bbh_cot_fewshot,gsm8k_cot \
+  --batch_size auto
 
 # HumanEval
 accelerate launch eval/bigcode-evaluation-harness/main.py \
@@ -28,4 +22,4 @@ accelerate launch eval/bigcode-evaluation-harness/main.py \
 GPU_COUNT=$(nvidia-smi --list-gpus 2>/dev/null | wc -l) || GPU_COUNT=0
 OUTPUT_FILE="outputs/alpacaeval/$(echo "$MODEL_PATH" | tr '/' '_').json"
 python -m eval.sample_for_alpacaeval "$MODEL_PATH" --gpu_count $GPU_COUNT --output_file "$OUTPUT_FILE"
-alpaca_eval evaluate --model_outputs="$OUTPUT_FILE" --is_overwrite_leaderboard=True
+alpaca_eval evaluate --is_overwrite_leaderboard=True --model_outputs="$OUTPUT_FILE"
--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+conda create --name halos python=3.10.14
+conda activate halos
+
 conda install pip
 pip install packaging ninja
 ninja --version
@@ -5,7 +10,22 @@ echo $?
 conda install pytorch=2.4.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install flash-attn==2.6.3 --no-build-isolation
 pip install transformers==4.44.0 
-pip install peft==0.12.0 datasets=2.20.0
+pip install peft==0.12.0
+pip install datasets==2.20.0
 pip install accelerate==0.33.0
 pip install vllm==0.5.5
-pip install wandb omegaconf openai hydra-core==1.3.2
+pip install alpaca-eval immutabledict langdetect wandb omegaconf openai hydra-core==1.3.2
+
+# lm-eval
+git clone --depth 1 https://github.com/EleutherAI/lm-evaluation-harness
+cd lm-evaluation-harness
+pip install -e .
+# download tasks for offline eval
+python << EOF
+from lm_eval import tasks
+task_names = ["winogrande", "mmlu", "gsm8k_cot", "bbh_cot_fewshot", "arc_easy", "arc_challenge", "hellaswag", "ifeval"]
+task_dict = tasks.get_task_dict(task_names)
+
+from datasets import load_dataset
+load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval")
+EOF

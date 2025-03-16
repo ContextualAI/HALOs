7e2a0e35e8ae55b55e3bff90385549a646c27269
sijial430 <lettiliu@gmail.com>
2025-03-16 00:14:55 -0400

    added wildbench

--- train/data.py
+++ train/data.py
@@ -572,3 +572,77 @@ def get_s1k_11(split: str = "train") -> Dataset:
         data[key].dataset_name = data.name
         data[key].sft_index = 0
     return data
+
+
+def get_safe_rlhf(split: str = "test") -> Dataset:
+    """
+    Load the PKU-Alignment PKU-SafeRLHF dataset from Huggingface and convert it into to a Dataset.
+    For this dataset, the SFT text is the preferred response.
+    
+    Args:
+        - split: one of 'test', 'train'. Using 'test' for evals by default.
+
+    Returns:   
+        A Dataset instance.
+    """
+    rank0_print(f'Loading PKU-SafeRLHF dataset ({split} split) from Huggingface...')
+    dataset = datasets.load_dataset('PKU-Alignment/PKU-SafeRLHF-single-dimension', split=split)
+    data = Dataset('safe_rlhf')
+
+    if on_rank0():
+        dataset = tqdm.tqdm(dataset, desc='Processing PKU-SafeRLHF-single-dimension')
+
+    for row in dataset:
+            # Create a unique key for this example (using the prompt)
+            conversation = [{"role": "user", "content": row['prompt']}]
+        
+            chosen_response_id = row['better_response_id']
+            chosen_response = row[f'response_{chosen_response_id}']
+            rejected_response = row[f'response_{1 - chosen_response_id}']
+            key = row["prompt"]
+
+            data[key].prompt = conversation
+            data[key].generations.append([{"role": "assistant", "content": chosen_response}])
+            data[key].generations.append([{"role": "assistant", "content": rejected_response}])
+            i, j = data[key].num_generations() - 2, data[key].num_generations() - 1
+            data[key].pairs.append((i, j))
+            data[key].sft_index = i
+            data[key].dataset_name = data.name
+            data[key].remove_extra_spaces()
+
+    return data
+
+
+def get_wildbench(split: str = "test") -> Dataset:
+    """
+    Load the AllenAI WildBench dataset from Huggingface and convert it into to a Dataset.
+    For this dataset, the SFT text is the preferred response.
+    
+    Args:
+        - split: 'v1-legacy', 'v2', 'v2-hard'. Using 'v2' by default.
+
+    Returns:   
+        A Dataset instance.
+    """
+    if split == 'test':
+        split = 'v2'
+        
+    rank0_print(f'Loading AllenAI WildBench dataset ({split} split) from Huggingface...')
+    dataset = datasets.load_dataset('allenai/WildBench', split)['test']
+    data = Dataset('wildbench')
+
+    if on_rank0():
+        dataset = tqdm.tqdm(dataset, desc='Processing WildBench')
+
+    for row in dataset:
+            # Create a unique key for this example (using the prompt)
+            conversation = [{"role": "user", "content": row['conversation_input'][0]['content']}]
+            key = row['conversation_input'][0]['content']
+
+            data[key].prompt = conversation
+            data[key].generations.append([{"role": "assistant", "content": row['references']['gpt-4']}])
+            data[key].sft_index = 0
+            data[key].dataset_name = data.name
+            data[key].remove_extra_spaces()
+
+    return data
